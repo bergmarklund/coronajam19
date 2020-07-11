c47721b7c8ba4f486959dff5a78516f6d335b15a
David Berg Marklund <dave@goblinbook>
2020-07-11 18:43:31 +0200

    moved stars closer

--- main.gd
+++ main.gd
@@ -8,7 +8,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$background.reload(4)
+	$background.reload(5)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
