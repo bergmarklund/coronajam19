0f97ee311844192f32dec9e063c9a89d2b94c41a
David Berg Marklund <dave@goblinbook>
2020-07-11 19:08:34 +0200

    made spaceship its own scene with background

--- main.gd
+++ main.gd
@@ -1,16 +1,11 @@
 extends Node
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$background.reload(5)
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
