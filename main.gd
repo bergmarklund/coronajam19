e44ebb2ca0991f16f107ba2779e5ce307de688b7
David Berg Marklund <dave@goblinbook>
2020-07-11 20:09:16 +0200

    can switch to nav console and fixed lightign for nav console

--- main.gd
+++ main.gd
@@ -1,11 +1,43 @@
 extends Node
 
-
-
-
-
+var spaceship_scene = preload("res://spaceship.tscn")
+var navconsole_scene = preload("res://navigation_console.tscn")
 # Called when the node enters the scene tree for the first time.
+
 func _ready():
-	pass
+	switch_to_scene(0)
+	
+func switch_to_scene(sceneid):
+	if sceneid == 0:
+		clear_current_scene()
+		goto_spaceship()
+	elif sceneid == 1:
+		clear_current_scene()
+		goto_nav_console()
+		
+func clear_current_scene():
+	if $current_scene.get_child_count() > 0:
+		var children = $current_scene.get_children()
+		for child in children:
+			child.queue_free()
+		
+func goto_spaceship():
+	var spaceship = spaceship_scene.instance()
+	spaceship.connect("display_nav_console", self, "_on_display_nav_console")
+	spaceship.connect("display_msg_console", self, "_on_display_msg_console")
+	$current_scene.add_child(spaceship)
+
+func goto_nav_console():
+	var nav_console = navconsole_scene.instance()
+	$current_scene.add_child(nav_console)
+
+
+func _on_display_nav_console():
+	switch_to_scene(1)
+	
+func _on_display_msg_console():
+	print("display msg")
+	
+
 
 
--- spaceship.gd
+++ spaceship.gd
@@ -3,14 +3,16 @@ signal display_nav_console
 signal display_msg_console
 
 var camera_start = 0
-# Called when the node enters the scene tree for the first time.
+var can_go_to_panels = true
 
 func _ready():
 	var start = $camera_origin
 	if camera_start == 1:
 		start = $camera_console_nav
+		can_go_to_panels = false
 	elif camera_start == 2:
 		start = $camera_console_msg
+		can_go_to_panels = false
 
 	$InterpolatedCamera.translation = start.translation
 	$InterpolatedCamera.rotation = start.rotation
@@ -31,3 +33,27 @@ func _on_nav_console_area_clicked():
 
 func _on_msg_console_area_clicked():
 	$InterpolatedCamera.set_target($camera_console_msg)
+
+
+func _on_nav_console_body_entered(body):
+	print(body)
+
+
+func _on_nav_console_area_entered(area):
+	if(area == $InterpolatedCamera/Area && can_go_to_panels):
+		emit_signal("display_nav_console")
+
+
+func _on_nav_console_area_exited(area):
+	if(area == $InterpolatedCamera/Area):
+		can_go_to_panels = true
+
+
+func _on_msg_console_area_entered(area):
+	if(area == $InterpolatedCamera/Area && can_go_to_panels):
+		emit_signal("display_msg_console")
+
+
+func _on_msg_console_area_exited(area):
+	if(area == $InterpolatedCamera/Area):
+		can_go_to_panels = true
