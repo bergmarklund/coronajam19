43467555e4244f47946f68d3742bc4063e1e6f0f
David Berg Marklund <dave@goblinbook>
2020-07-11 18:16:42 +0200

    rotating planets

--- main.gd
+++ main.gd
@@ -8,7 +8,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$background.reload(1)
+	$background.reload(5)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
