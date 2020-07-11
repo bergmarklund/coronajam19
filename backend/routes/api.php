f5c59b5733b07ed596fa52c4cfa7a39cd880dca6
Fernando Bevilacqua <dovyski@gmail.com>
2020-07-11 11:31:07 -0300

    Add basic endpoint to allow movement

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 // Join
 Route::post('/join', 'JoinController@index')->name('join.index');
 
+// Move
+Route::get('/{token}/{user}/move/{direction}', 'MoveController@index')->name('move.index');
+
 // Test
 Route::get('/test', 'TestController@index')->name('test.index');
 Route::get('/{token}/test', 'TestController@index')->name('test.index');
