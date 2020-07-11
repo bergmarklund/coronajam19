c07cad7c694f819a56a2355472707181508db2b8
David Berg Marklund <dave@goblinbook>
2020-07-11 20:27:36 +0200

    switch back and fortf between nav console and ship

--- main.gd
+++ main.gd
@@ -5,15 +5,8 @@ var navconsole_scene = preload("res://navigation_console.tscn")
 # Called when the node enters the scene tree for the first time.
 
 func _ready():
-	switch_to_scene(0)
-	
-func switch_to_scene(sceneid):
-	if sceneid == 0:
-		clear_current_scene()
-		goto_spaceship()
-	elif sceneid == 1:
-		clear_current_scene()
-		goto_nav_console()
+	goto_spaceship(0)
+
 		
 func clear_current_scene():
 	if $current_scene.get_child_count() > 0:
@@ -21,19 +14,26 @@ func clear_current_scene():
 		for child in children:
 			child.queue_free()
 		
-func goto_spaceship():
+func goto_spaceship(camera_start):
+	clear_current_scene()
 	var spaceship = spaceship_scene.instance()
 	spaceship.connect("display_nav_console", self, "_on_display_nav_console")
 	spaceship.connect("display_msg_console", self, "_on_display_msg_console")
+	spaceship.camera_start = camera_start
 	$current_scene.add_child(spaceship)
 
 func goto_nav_console():
+	clear_current_scene()
 	var nav_console = navconsole_scene.instance()
+	nav_console.connect("exit_nav_console", self, "_on_exit_nav_console")
 	$current_scene.add_child(nav_console)
 
 
 func _on_display_nav_console():
-	switch_to_scene(1)
+	goto_nav_console()
+	
+func _on_exit_nav_console():
+	goto_spaceship(1)
 	
 func _on_display_msg_console():
 	print("display msg")
