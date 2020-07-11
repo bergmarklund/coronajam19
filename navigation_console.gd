1c805dda9e30add69ba6001fe65070cb10001a55
Stolof <olof.marklund@valtech.com>
2020-07-11 17:34:49 +0200

    Added navigation by clicking arrows, and tracking on leds

--- navigation_console.gd
+++ navigation_console.gd
@@ -1,14 +1,31 @@
 extends Spatial
 
 var led = preload("res://led.tscn")
+var arrow = preload("res://navigation_arrow.tscn")
+var left_arrow = arrow.instance()
+
+var led_nodes = []
 var number_of_leds = 29
+var center_led_pos = Vector2((number_of_leds-1) / 2, (number_of_leds-1) / 2)
+
+# Colrs
+var green = Color(0.11,0.38,0.11)
+var yellow = Color(1,1,0)
 var red = Color(1,0,0)
 var blue = Color(0,0,1)
+#var green = Color(0,0,0)
+
+var navigation_led_color_state = 0
+var navigation_led_pos = center_led_pos
+var time = 0
 # var led_nodes = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	render_grid()
+	left_arrow = arrow.instance()
+	# green = get_led_color(1,1)
+	
 
 func render_grid():
 	for i in range(number_of_leds):
@@ -20,10 +37,24 @@ func render_led(x_pos, z_pos):
 	var led_node = led.instance()
 	led_node.set_translation(posistion)
 	if x_pos == number_of_leds / 2 and z_pos == number_of_leds / 2 :
-		print(posistion)
-		print(str(x_pos) + " " + str(z_pos))
 		led_node = change_led_color(led_node, red)
+	led_nodes.append(led_node)
 	self.add_child(led_node)
+	
+func blinking_navigation_led(pos_2d):
+	var led_node = get_led_by_pos(pos_2d)
+	var color = yellow
+	if navigation_led_color_state % 2 == 0:
+		color = green
+	change_led_color(led_node, color)
+	navigation_led_color_state += 1
+	
+func get_led_by_pos(pos_2d):
+	return led_nodes[ pos_2d.x*29 + pos_2d.y]
+	
+#func get_led_color(x_pos, z_pos):
+#	var led_node = get_led_by_pos(x_pos, z_pos)
+#	return led_node.get_child(2).get_surface_material()
 	
 func change_led_color(led_node, color):
 	var material = SpatialMaterial.new()
@@ -32,9 +63,54 @@ func change_led_color(led_node, color):
 		led_node.get_child(i).set_surface_material(0, material)
 	return led_node
 	
-
+func change_arrow_color(arrow_node, color):
+	var material = SpatialMaterial.new()
+	material.albedo_color = color
+	print(arrow_node)
+	print(arrow_node.get_child(0))
+	arrow_node.get_child(0).set_surface_material(0, material)
 
 	#print("created a node at pos " + str(posistion))
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(_delta):
+	time = time + _delta
+	if not compare_vector_pos(center_led_pos, navigation_led_pos) and int(time) % 2 == 1:
+		blinking_navigation_led(navigation_led_pos)
+		time = 0
+		
+func compare_vector_pos(vec1, vec2):
+	var same_pos = vec1.x == vec2.x and vec1.y == vec2.y
+	return same_pos
+	
+func _on_arrow_up_clicked():
+	if not compare_vector_pos(center_led_pos, navigation_led_pos):
+		change_led_color(get_led_by_pos(navigation_led_pos), green)
+	navigation_led_pos.y -= 1
+	
+func _on_arrow_right_area_clicked():
+	if not compare_vector_pos(center_led_pos, navigation_led_pos):
+		change_led_color(get_led_by_pos(navigation_led_pos), green)
+	navigation_led_pos.x += 1
+
+func _on_arrow_left_area_clicked():
+	if not compare_vector_pos(center_led_pos, navigation_led_pos):
+		change_led_color(get_led_by_pos(navigation_led_pos), green)
+	navigation_led_pos.x -= 1
+
+func _on_arrow_down_area_clicked():
+	if not compare_vector_pos(center_led_pos, navigation_led_pos):
+		change_led_color(get_led_by_pos(navigation_led_pos), green)
+	navigation_led_pos.y += 1
+
+
+func _on_arrow_right_mouse_entered():
+	change_arrow_color(get_child(0), yellow)
+
+func _on_arrow_left_mouse_entered():
+	change_arrow_color(get_child(1), yellow)
+
+func _on_arrow_up_mouse_entered():
+	change_arrow_color(get_child(2), yellow)
+
+func _on_arrow_down_mouse_entered():
+	change_arrow_color(get_child(3), yellow)
