86f8c06e1e2d55d162cc183af9df0706984f56be
Fernando Bevilacqua <dovyski@gmail.com>
2020-07-12 16:45:07 -0300

    Show items you have inside the ship

--- items_library.gd
+++ items_library.gd
@@ -9,12 +9,17 @@ var collectibles = {
 }
 
 func display_items(items):
-	print("displaying items")
+	for i in items:
+		show_item(i, items[i])
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
-
-	add_item(1, "collectible_alien")
+	return
+	
+func show_item(id, name):
+	if (has_item(id)):
+		return
+	add_item(id, name)
 	
 func add_item(id, name):
 	var collectible_resource = load(collectibles[name])
@@ -26,12 +31,26 @@ func add_item(id, name):
 	rng.set_seed(id)
 	rng.randomize()
 	
-	var offset_x = rng.randf_range(-2, 2)
-	var offset_z = rng.randf_range(-2, 2)
-	collectible_item.translate(Vector3(offset_x, 0, offset_z))
+	var offset_x = rng.randf_range(-1.0, 2.0)
+	var offset_y = rng.randf_range(0, 2.0)
+	var offset_z = rng.randf_range(-1.8, 1.8)
+	
+	collectible_item.translate(Vector3(offset_x, offset_y, offset_z))
+	collectible_item.rotate_x(rng.randf_range(0, 90))
+	collectible_item.rotate_y(rng.randf_range(0, 90))
+	collectible_item.rotate_z(rng.randf_range(0, 90))
 	
 	add_child(collectible_item)
 	
+func has_item(id):
+	for node in get_children():
+		if (node.get_name() == String(id)):
+			return true
+	return false
+
 func _process(delta):
 	for node in get_children():
-		node.rotate_y(1 * delta)
+		if( int(node.get_name()) % 2 == 0):
+			node.rotate_y(0.1 * delta)
+		else:
+			node.rotate_y(0.8 * delta)
