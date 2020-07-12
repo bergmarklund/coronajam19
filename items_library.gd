04404f65dd7bd32668d225940a5fe91ffa500f8d
Fernando Bevilacqua <dovyski@gmail.com>
2020-07-12 15:32:00 -0300

    Add basic items library (place to show items inside the ship)

---
+++ items_library.gd
@@ -0,0 +1,19 @@
+extends Spatial
+
+
+var collectibles = {
+	"collectible_alien": preload("res://assets/sounds/communication/receive/A_low.wav"),
+	"collectible_hot_carrot": preload("res://assets/sounds/communication/receive/B_low.wav"),
+	"collectible_plant_palm": preload("res://assets/sounds/communication/receive/C_low.wav"),
+	"collectible_plant_small": preload("res://assets/sounds/communication/receive/D_high.wav"),
+	"collectible_plant_tree": preload("res://assets/sounds/communication/receive/D_low.wav"),
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
