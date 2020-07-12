909cac0bc5e08fa0fdcd517ba9a277258e50eb2f
Stolof <olof.marklund@valtech.com>
2020-07-12 23:28:03 +0200

    Added new buttons for send and play

--- music_button.gd
+++ music_button.gd
@@ -4,6 +4,7 @@ signal music_button_clicked(id)
 var id = 0
 var hover_yellow = Color(1,1,0)
 var red = Color(1,0,0)
+var green = Color(0,1,0)
 
 var timer = null
 var button_pressed = false
@@ -17,7 +18,14 @@ func _ready():
 func change_color(color):
 	var material = SpatialMaterial.new()
 	material.albedo_color = color
-	get_child(0).set_surface_material(0, material)
+	var last_child_index = get_child_count() - 1
+	var child_index = 1
+	for child in get_children():
+		print(last_child_index)
+		if child_index == last_child_index:
+			return	
+		child.set_surface_material(0, material)
+		child_index += 1
 
 func _on_music_button_mouse_entered():
 	change_color(hover_yellow)
@@ -40,3 +48,28 @@ func lower_button():
 	pos.y += 0.2
 	self.transform.origin = pos
 	button_pressed = false
+
+func _on_send_area_clicked():
+	if !button_pressed:
+		lower_button()
+	emit_signal("music_button_clicked", id)
+
+func _on_send_mouse_entered():
+	change_color(hover_yellow)
+
+func _on_send_mouse_exited():
+	change_color(Color("7f1212"))
+
+func _on_play_area_clicked():
+	if !button_pressed:
+		lower_button()
+	emit_signal("music_button_clicked", id)
+
+func _on_play_mouse_entered():
+	change_color(hover_yellow)
+
+func _on_play_mouse_exited():
+	change_color(Color("0a760a"))
+
+func _on_music_panel_send_ready():
+	change_color(Color("7f1212"))
