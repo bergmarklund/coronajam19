e3296e132946af0c4a173d825d5e3f72a83ffba0
Stolof <olof.marklund@valtech.com>
2020-07-12 13:55:09 +0200

    changed led pos to 2d array

--- navigation_console.gd
+++ navigation_console.gd
@@ -4,19 +4,13 @@ signal exit_nav_console
 # Variables
 var led = preload("res://led.tscn")
 var led_nodes = []
-var led_nodes_pos = [[]]
 var number_of_leds = 29
 var center_led_pos = Vector2((number_of_leds-1) / 2, (number_of_leds-1) / 2)
 var navigation_led_pos = center_led_pos
-var navigation_led_color_state = 0
-var time = 0
 
 # Colors
 var green = Color(0.11,0.38,0.11)
-var yellow = Color(1,1,0)
 var red = Color(1,0,0)
-var blue = Color(0,0,1)
-
 var hover_yellow = Color(1,1,0)
 var arrow_grey = Color(0.41, 0.41, 0.41)
 var launch_red = Color(1,0,0)
@@ -27,7 +21,6 @@ func _input(event):
 		if(event.pressed && event.button_index == BUTTON_RIGHT):
 			emit_signal("exit_nav_console")
 
-
 func _ready():
 	render_grid()
 	fix_button_colors()
@@ -36,24 +29,26 @@ func fix_button_colors():
 	change_arrow_color(get_child(4), red)
 	for i in range(4):
 		change_arrow_color(get_child(i), arrow_grey)
-		
 
 func render_grid():
 	for i in range(number_of_leds):
+		var led_array = []
 		for j in range(number_of_leds):
-			render_led(i,j)
+			var led_node = render_led(i,j)
+			led_array.append(led_node)
+		led_nodes.append(led_array)
 
 func render_led(x_pos, z_pos):
 	var posistion = Vector3(x_pos - ((number_of_leds-1)/2), 1.25, z_pos - ((number_of_leds-1)/2))
 	var led_node = led.instance()
 	led_node.set_translation(posistion)
-	led_nodes.append(led_node)
 	self.add_child(led_node)
 	if x_pos == number_of_leds / 2 and z_pos == number_of_leds / 2 :
 		led_node.change_led_color(red)
+	return led_node
 
 func get_led_by_pos(pos_2d):
-	return led_nodes[pos_2d.x*29 + pos_2d.y]
+	return led_nodes[pos_2d.x][pos_2d.y]
 
 func change_arrow_color(arrow_node, color):
 	var material = SpatialMaterial.new()
@@ -73,7 +68,8 @@ func led_switch(navigation_led_pos, status):
 		else:
 			led_node.disable_navigation_led()
 
-# Hover and click functions
+
+### Hover and click functions ###
 func _on_arrow_up_clicked():
 	led_switch(navigation_led_pos, false)
 	navigation_led_pos.y -= 1
@@ -95,16 +91,16 @@ func _on_arrow_down_area_clicked():
 	led_switch(navigation_led_pos, true)
 
 func _on_arrow_right_mouse_entered():
-	change_arrow_color(get_child(0), yellow)
+	change_arrow_color(get_child(0), hover_yellow)
 
 func _on_arrow_left_mouse_entered():
-	change_arrow_color(get_child(1), yellow)
+	change_arrow_color(get_child(1), hover_yellow)
 
 func _on_arrow_up_mouse_entered():
-	change_arrow_color(get_child(2), yellow)
+	change_arrow_color(get_child(2), hover_yellow)
 
 func _on_arrow_down_mouse_entered():
-	change_arrow_color(get_child(3), yellow)
+	change_arrow_color(get_child(3), hover_yellow)
 
 func _on_arrow_right_mouse_exited():
 	change_arrow_color(get_child(0), arrow_grey)
@@ -119,7 +115,7 @@ func _on_arrow_down_mouse_exited():
 	change_arrow_color(get_child(3), arrow_grey)
 
 func _on_launch_button_mouse_entered():
-	change_arrow_color(get_child(4), yellow)
+	change_arrow_color(get_child(4), hover_yellow)
 
 func _on_launch_button_mouse_exited():
 	change_arrow_color(get_child(4), red)
